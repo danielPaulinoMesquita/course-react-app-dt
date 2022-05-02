974f5367536bbc5ae7e8faa8df065b7a0c8484de
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-05-02 01:51:42 -0300

    3- Create GlobalStyle and configured a new font style from Google font.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import styled from "styled-components";
+import {GlobalStyle} from "./styles/global";
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: darkorchid;
-  p {
-    font-size: 12px;
-    color: aqua;
-  }
-`
 
 export function App() {
   return (
-    <Title>
-       Hello world <p>TESTANDO DENTRO DO STYLED</p>
-    </Title>
+      <>
+        <h1>Hello world</h1>
+        <GlobalStyle/>
+      </>
   );
 }
 
