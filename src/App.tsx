8ab65998fa415797c367cf746613cc96e002979d
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-05-03 01:58:26 -0300

    5- Create more two components Dashboard and Summary.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {GlobalStyle} from "./styles/global";
 import {Header} from "./components/Header";
+import {Dashboard} from "./components/Dashboard";
 
 
 export function App() {
     return (
         <>
             <Header/>
+            <Dashboard/>
             <h1>Hello world</h1>
             <GlobalStyle/>
         </>
