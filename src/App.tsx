57f2914372a39191a1dd0828537aad2a57e5bce6
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-05-07 02:00:18 -0300

    9- Added Modal in application and passing properties through the components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {GlobalStyle} from "./styles/global";
 import {Header} from "./components/Header";
 import {Dashboard} from "./components/Dashboard";
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <>
-            <Header/>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
             <Dashboard/>
+            <Modal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}>
+                <h2>Cadastro Transação</h2>
+            </Modal>
             <GlobalStyle/>
         </>
     );
